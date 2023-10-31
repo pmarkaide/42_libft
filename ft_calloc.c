31fbd35f04b6dc9a2f0ae526c868307b7c71fa21
Pablo Markaide <pmarkaid@student.hive.fi>
2023-10-31 14:59:08 +0200

    CHECKPOINT: part1 pasing for first tester after fixing strlcat

--- ft_calloc.c
+++ ft_calloc.c
@@ -14,12 +14,12 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	size_t total_size;
-	void *ptr;
-	
+	size_t	total_size;
+	void	*ptr;
+
 	total_size = count * size;
 	ptr = malloc(total_size);
 	if (ptr != NULL)
 		ft_bzero(ptr, total_size);
-	return ptr;
+	return (ptr);
 }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 10:26:16 by pmarkaid          #+#    #+#             */
-/*   Updated: 2023/10/30 16:26:23 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2023/10/31 14:57:16 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,14 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	size_t	dst_len;
 	size_t	src_len;
-	size_t	i;
-	size_t	j;
 
+	if (!dstsize)
+		return (0);
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
-	if (!dst || !src || dstsize == 0)
-		return (src_len);
 	if (dst_len >= dstsize)
 		return (dstsize + src_len);
-	i = 0;
-	j = dst_len;
-	while (src[i] && dst_len + i < dstsize - 1)
-		dst[j++] = src[i++];
-	dst[j] = '\0';
+	dst = &dst[dst_len];
+	ft_strlcpy(dst, src, dstsize - dst_len);
 	return (dst_len + src_len);
 }
