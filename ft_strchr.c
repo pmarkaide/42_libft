2af904d8c35a11f851eb2ca9590043e3d009a43b
Pablo Markaide <pmarkaid@student.hive.fi>
2023-11-08 11:32:49 +0200

    fix: type cast the int to char to handle non-ascii values in strchr and strrchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 10:41:38 by pmarkaid          #+#    #+#             */
-/*   Updated: 2023/10/30 16:39:28 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2023/11/08 11:23:18 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	if (c == '\0')
+	if ((char)c == '\0')
 		return ((char *)(s + ft_strlen(s)));
 	while (*s)
 	{
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 13:57:33 by pmarkaid          #+#    #+#             */
-/*   Updated: 2023/10/27 09:26:50 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2023/11/08 11:30:45 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strrchr(const char *s, int c)
 	char	*last;
 
 	last = NULL;
-	if (c == '\0')
+	if ((char)c == '\0')
 		return ((char *)(s + ft_strlen(s)));
 	while (*s)
 	{
