2f46876b2a697cdfca9c29ec1c98d745fd190134
Pablo Markaide <pmarkaid@student.hive.fi>
2023-11-10 12:36:35 +0200

    feat: lstmap will clean the new list if something goes wrong

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:50:10 by pmarkaid          #+#    #+#             */
-/*   Updated: 2023/11/07 10:31:31 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2023/11/10 12:32:40 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	while (lst)
 	{
 		new_node = ft_lstnew(f(lst->content));
+		if (!(new_node))
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
 	}
